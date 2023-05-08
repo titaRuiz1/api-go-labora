330965e688da63f3af35737c42510fd2170ab68b
Tita Ruiz <asuntita.rl@gmail.com>
2023-05-07 23:48:31 -0500

    add:insert data sql

---
+++ sql/insert_and_select.sql
@@ -0,0 +1,10 @@
+-- \c labora_proyect_1
+
+INSERT INTO items (customer_name, order_date, product, quantity, price)
+VALUES ('Jesus', '2023-05-07', 'Coca-Cola', 2, 1.5),
+       ('Jorge', '2023-05-08', 'Pepsi', 3, 2.0),
+       ('Luis', '2023-05-08', 'Sprite', 4, 2.5),
+       ('Ximena', '2023-05-09', 'Fanta', 1, 1.0),
+       ('Sara', '2023-05-09', 'Agua Mineral', 6, 1.0);
+
+-- SELECT *FROM items;       
